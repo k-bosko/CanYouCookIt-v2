3f370ff12f3b5e0fca7e8b8adf7c7728ec7e9105
Anchellon <anshulmathew@gmail.com>
2022-11-29 10:59:24 -0800

    Update mongoexport.sh

--- db/exportDB/mongoexport.sh
+++ db/exportDB/mongoexport.sh
@@ -4,3 +4,4 @@ mongoexport --collection=recipes --db=CanYouCookIt --jsonArray --out=./db/export
 mongoexport --collection=myrecipes --db=CanYouCookIt --jsonArray --out=./db/exportDB/json/myrecipes.json
 mongoexport --collection=ingredients --db=CanYouCookIt --jsonArray --out=./db/exportDB/json/ingredients.json
 mongoexport --collection=inventory --db=CanYouCookIt --jsonArray --out=./db/exportDB/json/inventory.json
+mongoexport --collection=auto-increments --db=CanYouCookIt --jsonArray --out=./db/exportDB/json/auto-increments.json
