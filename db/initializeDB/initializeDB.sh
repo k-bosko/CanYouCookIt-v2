cac0ca843a766b9949afbfca6103d42083e09905
anchellon <anshulmathew@gmail.com>
2022-11-29 10:49:41 -0800

    added db info

--- db/initializeDB/initializeDB.sh
+++ db/initializeDB/initializeDB.sh
@@ -4,4 +4,5 @@ mongoimport -h localhost:27017 -d CanYouCookIt -c recipes --jsonArray --drop ./d
 mongoimport -h localhost:27017 -d CanYouCookIt -c myrecipes --jsonArray --drop ./db/exportDB/json/myrecipes.json
 mongoimport -h localhost:27017 -d CanYouCookIt -c ingredients --jsonArray --drop ./db/exportDB/json/ingredients.json
 mongoimport -h localhost:27017 -d CanYouCookIt -c inventory --jsonArray --drop ./db/exportDB/json/inventory.json
+mongoimport -h localhost:27017 -d CanYouCookIt -c auto-increments --jsonArray --drop ./db/exportDB/json/auto-increments.json
 
