1e41bffa60e1fe9383f2ac0152922a10d571bf4b
Katerina Bosko <katerinabosko@gmail.com>
2022-12-06 23:29:47 -0800

    made Recipe card keyboard accessible

--- front/src/components/recipes/RecipeCard.jsx
+++ front/src/components/recipes/RecipeCard.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React from "react";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 import "./RecipeCard.css";
 
 RecipeCard.propTypes = {
@@ -10,43 +11,31 @@ RecipeCard.propTypes = {
 };
 
 function RecipeCard(props) {
-  const [isMouseOver, setMouseOver] = useState(false);
-
-  function handleMouseOver() {
-    setMouseOver(true);
-  }
-
-  function handleMouseOut() {
-    setMouseOver(false);
-  }
-
   return (
     <div
       className="card me-5 mb-5"
-      style={{
-        border: isMouseOver ? "1px solid red" : "1px solid lightgrey",
-        width: "15rem",
-      }}
-      onMouseOver={handleMouseOver}
-      onMouseOut={handleMouseOut}
       onClick={() => {
         props.isMyRecipesPage
           ? props.setCurrentRecipe(props.recipe)
           : props.showRecipeDetails(props.recipe.id);
       }}
     >
-      <div className="card-body">
-        <img
-          src={props.recipe.image}
-          alt={props.recipe.title}
-          className="RecipeCardImage mb-4"
-        />
-        <p className="RecipeCardTitle">
-          {props.recipe.title.length > 30
-            ? props.recipe.title.substring(0, 30) + "..."
-            : props.recipe.title}
-        </p>
-      </div>
+      <Link
+        className="stretched-link no-decor"
+      >
+        <div className="card-body">
+          <img
+            src={props.recipe.image}
+            alt={props.recipe.title}
+            className="RecipeCardImage mb-4"
+          />
+          <p className="RecipeCardTitle">
+            {props.recipe.title.length > 30
+              ? props.recipe.title.substring(0, 30) + "..."
+              : props.recipe.title}
+          </p>
+        </div>
+      </Link>
     </div>
   );
 }
